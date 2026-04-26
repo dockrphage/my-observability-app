8b5704d4c8d052f8bc3eb981ffac99918b7907b2
dockrphage <villers118@yahoo.com>
2026-04-26 22:58:44 +0100

    deleted deployment-snapshot dir

--- deployment-snapshot/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Stopping all services..."
-docker stop app proxy prometheus grafana loki promtail 2>/dev/null
-docker rm app proxy prometheus grafana loki promtail 2>/dev/null
-echo "All services stopped!"
